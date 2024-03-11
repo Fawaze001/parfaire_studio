d070072d1251a56c43a71f9cab89b16121ade432
Sabino5000 <sabinosarobase@gmail.com>
2024-03-11 14:12:50 +0100

    page 2 debut

---
+++ page2.php
@@ -0,0 +1,86 @@
+<?php include('partials/header.php') ?>
+
+<!-- page2 -->
+
+<section class="page2banner container-fluid">
+    <div class="row">
+        <div class="offset-md-7 col-md-5 col-sm-12">
+            <div>
+                <div class="titre1 text-center">
+                    <p class="first-title1">Parfaire</p>
+                </div>
+                <div class="farçonner">
+                    Farçonner l'exellence
+                </div>
+                <div class="tres11"></div> <br>
+                <div class="tres22"></div> <br>
+                <div class="tres33"></div>
+
+                <div class="harpon1">
+                    <button class="btn-style1 ">je configure mon harpon</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- fin premiere setion -->
+<!-- debut deuxième setion -->
+<section class="rectangle container-fluid">
+    <div class="row">
+        <div class="col-md-12 col-sm-12 text-center">
+            <img class="img-fluid w-50" src="assets\img\emotionby.png" id="image" alt="emotion">
+        </div>
+    </div>
+
+
+    <div class="container" id="doublesan">
+        <p class=" text-white">MONO ROLLER</p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 col-sm-12 align_text text-center">
+            <p>
+                Portée : 4,5m &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Maniabilité : 9/10 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Simplicité 10/10&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Recul 8/10&nbsp;&nbsp;&nbsp;&nbsp;
+                Précision 10/10
+            </p>
+        </div>
+    </div>
+
+    <div class="row py-4 mx-4">
+        <div class="col-md-12 col-sm-12 text-center">
+            <img width="" src="assets\img\emotion.png" alt="Pretention" class="img-fluid mx-4 px-4 image">
+            
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-md-12 text-center ">
+            <p class="textgift1">/ GIFT SPLINE /</p>
+        </div>
+    </div> 
+
+
+    <div class="row">
+        <div class="col-md-12 col-md-12 text-center ">
+            <p class="textnextgift">Le fusil parfait pour le chasseur qui souhaite prospecter les</p>
+                <p class="textnextgift1"> fonds 
+                marins à l'indienne et à l'agachon</p>
+        </div>
+    </div>
+</section>
+
+<section class="rectangle1 container-fluid">
+  aaaaaaaaa
+</section>
+<!-- fin deuxieme setion -->
+
+<section class="section1 container-fluid">
+
+</section>
+
+
+
+<?php include('partials/footer.php') ?>
